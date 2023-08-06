97c653a8e01ee33ead0f57b91a8d0b1a067dadc1
Kayinda Immaculé <immakaymw@gmail.com>
2023-08-06 16:21:08 +0100

    Before removing migrations files.

--- back-end/django_slack/urls.py
+++ back-end/django_slack/urls.py
@@ -18,11 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("admin/", admin.site.urls),
     path("teams/", include("teams.urls")),
     path("tasks/", include("tasks.urls")),
-    path("api-auth/", include("rest_framework.urls")),
-    path("users/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("users/dj-rest-auth/", include("dj_rest_auth.registration.urls")),
 ]
--- back-end/users/urls.py
+++ back-end/users/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import UserList, UserDetail
 
 urlpatterns = [
     path("list/", UserList.as_view(), name="user-list"),
-    path("<int:pk>/", UserDetail.as_view(), name="user-detail"),
+    path("int:pk>/", UserDetail.as_view(), name="user-detail"),
+    path("api-auth/", include("rest_framework.urls")),
+    path("dj-rest/auth/", include("dj_rest_auth.urls")),
+    path("dj-rest/registration/", include("dj_rest_auth.registration.urls")),
 ]
